83a9b97602efbb2d2c243518a74c17084859fc3e
Fajrin Kingwijati <fajrin.kingwijati@traveloka.com>
2023-11-28 15:41:34 +0700

    Add java function and result (#3)

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# go-vs-java-concurrency
+# Go vs Java Concurrency
+
+We run 1000 tasks concurrently and measure the time it takes to complete.
+
+## Go
+```bash
+Total time taken: 101.431708ms
+./main  0.00s user 0.00s system 8% cpu 0.109 total
+```
+
+## Java with Platform Thread
+
+1000 ExecutorService threads are created and run concurrently.
+
+```bash
+Total time taken:: 179ms
+java JavaConcurrentClient  0.23s user 0.34s system 193% cpu 0.296 total
+```
+
+## Java with Virtual Thread
+
+Will need JDK 21 or later.
+
+```bash
+Total time taken:: 124ms
+java JavaConcurrentClient  0.14s user 0.02s system 85% cpu 0.195 total
+```
---
+++ java-platform-thread/JavaConcurrentClient.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class JavaConcurrentClient {
+    public static void main(String[] args) {
+        int numberOfRequests = 1000; // Change this as needed
+
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfRequests);
+
+        // Create an array of CompletableFuture objects
+        CompletableFuture<Void>[] futures = new CompletableFuture[numberOfRequests];
+
+        // Log the duration of this operation
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < numberOfRequests; i++) {
+            // Asynchronously run the simulateDelay function for each request using the custom executor
+            futures[i] = CompletableFuture.runAsync(() -> simulateDelay(), executor);
+        }
+
+        // Combine all CompletableFuture objects into a single CompletableFuture
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures);
+
+        try {
+            // Wait for all CompletableFuture tasks to complete
+            allOf.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            // Shutdown the executor to release resources
+            executor.shutdown();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("All requests completed in: " + (endTime - startTime) + "ms");
+        System.out.println("All requests completed");
+    }
+
+    private static void simulateDelay() {
+        // Simulate a delay (100ms)
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+    
+
+    
